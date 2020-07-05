7fb2d2596eae1730e5c787c15b92f041b281bc57
rky91 <rakeshkumar.jena91@gmail.com>
2020-07-05 19:49:08 +0530

    initial commit
    Signed-off-by: rky91 <rakeshkumar.jena91@gmail.com>

---
+++ src/main/java/com/rky/dev/AzureWebappHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.rky.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureWebappHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureWebappHelloWorldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rky/dev/AzureWebappHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rky.dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureWebappHelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
